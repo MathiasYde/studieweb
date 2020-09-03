9ce947cf3ee84b87b4cbecdf3b14921b950a11b2
MathiasYde <35201354+MathiasYde@users.noreply.github.com>
2020-09-03 11:42:41 +0200

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      fontFamily: {
+        'roboto': ['roboto', 'sans-serif']
+      },
+    },
+  },
+  variants: {},
+  plugins: [
+    require('@tailwindcss/ui'),
+  ],
+}
