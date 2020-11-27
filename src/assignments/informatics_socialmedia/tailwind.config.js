66dd85598dc68b57c421d9005886c0efcd5624ec
MathiasYde <35201354+MathiasYde@users.noreply.github.com>
2020-11-27 10:31:14 +0100

    Added informatics assignment

---
+++ src/assignments/informatics_socialmedia/tailwind.config.js
@@ -0,0 +1,62 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      height: {
+        "screen-1/2": "50vh",
+        "screen-2/2": "100vh",
+
+        "screen-1/4": "25vh",
+        "screen-2/4": "50vh",
+        "screen-3/4": "75vh",
+        "screen-4/4": "100vh",
+
+        "screen-1/6": "16.6vh",
+        "screen-2/6": "33.3vh",
+        "screen-3/6": "50vh",
+        "screen-4/6": "66.6vh",
+        "screen-5/6": "83.3vh",
+        "screen-6/6": "100vh",
+
+        "screen-1/8": "12.5vh",
+        "screen-2/8": "25vh",
+        "screen-3/8": "37.5vh",
+        "screen-4/8": "50vh",
+        "screen-5/8": "62.5vh",
+        "screen-6/8": "75vh",
+        "screen-7/8": "87.5vh",
+        "screen-8/8": "100vh"
+      },
+      width: {
+        "screen-1/2": "50vw",
+        "screen-2/2": "100vw",
+
+        "screen-1/4": "25vw",
+        "screen-2/4": "50vw",
+        "screen-3/4": "75vw",
+        "screen-4/4": "100vw",
+
+        "screen-1/6": "16.6vw",
+        "screen-2/6": "33.3vw",
+        "screen-3/6": "50vw",
+        "screen-4/6": "66.6vw",
+        "screen-5/6": "83.3vw",
+        "screen-6/6": "100vw",
+
+        "screen-1/8": "12.5vw",
+        "screen-2/8": "25vw",
+        "screen-3/8": "37.5vw",
+        "screen-4/8": "50vw",
+        "screen-5/8": "62.5vw",
+        "screen-6/8": "75vw",
+        "screen-7/8": "87.5vw",
+        "screen-8/8": "100vw"
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
